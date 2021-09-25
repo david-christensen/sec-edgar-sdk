409731f4e6d47b10629e850a3d3f0aaf126955f0
David Christensen <david.christensen@paymentspring.com>
2021-09-25 15:13:22 -0500

    tests for SecEdgar::FullTextSearch

--- spec/sec-edgar/full_text_search_spec.rb
+++ spec/sec-edgar/full_text_search_spec.rb
@@ -8,8 +8,34 @@ RSpec.describe SecEdgar::FullTextSearch do
 
     let(:search_response) { subject.to_h }
 
-    it { is_expected.to be_success }
+    around do |example|
+      VCR.use_cassette('efts-brk') do
+        example.run
+      end
+    end
 
+    it { is_expected.to be_success }
     specify { expect(search_response).to be_a Hash }
+    specify do
+      expect(search_response).to match(
+                                   hash_including(
+                                     took: a_kind_of(Integer),
+                                     timed_out: false,
+                                     _shards: { total: 1, successful: 1, skipped: 0, failed: 0 },
+                                     hits: hash_including(
+                                       total: { value: a_kind_of(Integer), relation: 'eq' },
+                                       hits: array_including(
+                                         hash_including(
+                                           _index: a_kind_of(String),
+                                           _type: a_kind_of(String),
+                                           _id: a_kind_of(String),
+                                           _score: a_kind_of(Float),
+                                           _source: a_kind_of(Hash)
+                                         )
+                                       )
+                                     )
+                                   )
+                                 )
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
 require 'pry'
+require 'active_support/time'
 
 require_relative '../lib/sec-edgar-sdk'
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.hook_into :webmock
+  c.cassette_library_dir = 'cassettes'
+  c.configure_rspec_metadata!
+  c.default_cassette_options = {
+    record: :once,
+    match_requests_on: %i[method uri body]
+  }
+end
